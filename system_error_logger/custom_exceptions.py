fab82aafc9f3db99ac2fe8e96952b04e5ba4e9de
Duke Buerano <dukebueranoo@gmail.com>
2026-04-29 10:01:31 +0800

    nagdagdag ng write_error method na may datetime formatter at append mode para sa file object

--- system_error_logger/custom_exceptions.py
+++ system_error_logger/custom_exceptions.py
@@ -4,3 +4,7 @@ class BaseLogger:
 
     def __init__(self, log_file_name="error_logs.txt"):
         self.log_file_name = log_file_name
+    def write_error(self, error_message):
+        timestamp_string = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        with open(self.log_file_name, "a") as log_file_object:
+            log_file_object.write(f"[{timestamp_string}] RUNTIME ERROR: {error_message}\n")
